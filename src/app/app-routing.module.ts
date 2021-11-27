7ed95d5fae62ec3c359ce2ef8d41c5d38495b344
backnx <bachnx@enterprisenao.com>
2021-11-27 20:51:08 +0700

    comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PostsComponent } from './posts/posts.component';
-import { PostDetailComponent } from './post-detail/post-detail.component';
-import { IndexComponent } from './common/index/index.component';
-import { SignInComponent } from './authentication/sign-in/sign-in.component';
-import { SignUpComponent } from './authentication/sign-up/sign-up.component';
-import { ForgotPasswordComponent } from './authentication/forgot-password/forgot-password.component';
+import { PostsComponent } from './front-end/post/posts/posts.component';
+import { PostDetailComponent } from './front-end/post/post-detail/post-detail.component';
+import { IndexComponent } from './front-end/base/index/index.component';
+import { SignInComponent } from './front-end/authentication/sign-in/sign-in.component';
+import { SignUpComponent } from './front-end/authentication/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './front-end/authentication/forgot-password/forgot-password.component';
 const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
   {
