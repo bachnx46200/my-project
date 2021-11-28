02bf57d86efe2801e6e3c5467328fc5537c24123
backnx <bachnx@enterprisenao.com>
2021-11-28 15:56:05 +0700

    jhg
    Signed-off-by: backnx <bachnx@enterprisenao.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,11 +8,19 @@ import { vi_VN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import vi from '@angular/common/locales/vi';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgZorroAntdModule } from './NgZorroAntdModule .module';
 import { FrontEndModule } from './front-end/frontEnd.module';
+import { TranslateModule, TranslateService, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 registerLocaleData(vi);
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 
 @NgModule({
   declarations: [
@@ -25,10 +33,23 @@ registerLocaleData(vi);
     HttpClientModule,
     NgZorroAntdModule,
     BrowserAnimationsModule,
-    FrontEndModule
+    FrontEndModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
-  providers: [{ provide: NZ_I18N, useValue: vi_VN },
-],
+  providers: [{
+    provide:
+      NZ_I18N,
+    useValue: vi_VN,
+  },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
